25a80488baa4faf700ded8feab5d491e00dea2ea
vipul <vipulshah1395@gmail.com>
2020-04-15 19:29:58 +0530

    my project

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template,url_for
+import os
+app = Flask(__name__,static_url_path="",static_folder="",template_folder="/home/vipul/Documents/pdfflask-deploy/templates")
+global directory
+path = '/home/vipul/Documents/pdfflask-deploy/ram'
+directory = os.listdir(path)
+s="ram/"
+li=[]
+for var in directory:
+    li.append(s+var)
+@app.route("/")
+def home():
+    return render_template('test.html', li=li,value=len(li))
+if(__name__=="__main__"):
+    app.run()
