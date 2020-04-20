ea9b516d2283bdcaaa79213ca28270c74c1a1e5c
vipul1396 <63605855+vipul1396@users.noreply.github.com>
2020-04-20 14:43:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,url_for
 import os
 app = Flask(__name__,static_url_path="",static_folder="",template_folder="/home/vipul/Documents/pdfflask-deploy/templates")
 global directory
-path = '/home/vipul/Documents/pdfflask-deploy/ram'
+path = '/ram'
 directory = os.listdir(path)
 s="ram/"
 li=[]
